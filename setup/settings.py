5cc60b4716710dcf8a6d61d1ace1c10ea6164ec7
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2024-10-26 11:04:03 -0300

    Criando o app enquetes e navegação entre as telas

--- setup/settings.py
+++ setup/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'core',
+    'enquetes',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- setup/urls.py
+++ setup/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import core
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('enquetes/', include('enquetes.urls')),
 ]
